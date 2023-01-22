7c9365d020365db3b016ba2c8a84f88cceafc6af
Marin <marin.getejanc.upt@gmail.com>
2023-01-22 17:17:45 +0200

    Added S3 storage for the backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "agrodetectbc12c35a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "agrodetectInferenceImages": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
