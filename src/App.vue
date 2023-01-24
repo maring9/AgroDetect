e2149eed135136dbf1657ae762fae5c6ccf7aa66
Marin <marin.getejanc.upt@gmail.com>
2023-01-24 13:40:08 +0200

    Added build dir

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import '@aws-amplify/ui-vue/styles.css';
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!!" />
     </div>
   </header>
 
