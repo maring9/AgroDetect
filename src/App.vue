2a7434902c76f2bf28ac83389f4532dabc4db11e
Marin <marin.getejanc.upt@gmail.com>
2023-02-14 08:07:41 +0200

    Added logo

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TestGraphQL from './components/TestGraphQL.vue';
 
 <template>
   <header>
-    <img alt="AgroDetectLogo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="AgroDetectLogo" class="logo" src="./assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!!" />
