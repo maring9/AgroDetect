510e3c9fefae67a530c945010e6b45b9de8b030b
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 23:39:19 +0200

    Modified app to use auth page wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { Authenticator } from '@aws-amplify/ui-vue';
-import Inference from './components/Inference.vue';
-import '@aws-amplify/ui-vue/styles.css';
-import TestGraphQL from './components/TestGraphQL.vue';
+<script>
+import AuthenticationPage from './components/pages/auth/AuthenticationPage.vue';
+
+export default{
+  components: {
+    AuthenticationPage
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="AgroDetectLogo" class="logo" src="./assets/logo.png" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!!" />
-    </div>
-  </header>
 
   <main>
-    <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <TestGraphQL></TestGraphQL>
-      <Inference></Inference>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator>
+    <AuthenticationPage></AuthenticationPage>
   </main>
 </template>
 
