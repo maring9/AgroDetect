5aa36cd973c13bf721bbdefd8c425c11e7b038a8
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 15:40:42 +0200

    Deleted css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
-
 import { Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
