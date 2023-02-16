fe259c4158a130dd2cd5e901d73ff88b24f678eb
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 23:37:43 +0200

    Added header and footer scheleton

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+      <p>Copyright &copy; 2023 AgroDetect</p>
+      <a href="#"> Terms Of Service</a>
+      <bn/>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: #3d008d;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
