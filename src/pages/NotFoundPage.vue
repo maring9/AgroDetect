f80e9f43367e2641579d2f2303d4b3216325fe82
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 09:06:43 +0200

    Added base page components

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+      <base-card>
+        <h2>Page not found</h2>
+        <p>
+          Go to 
+          <router-link to="/home">Home</router-link>.
+        </p>
+      </base-card>
+    </section>
+  </template>
