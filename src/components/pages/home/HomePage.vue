1317fb3e53cc80cb4ddc32b61043896f163e9b00
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 23:38:20 +0200

    Add home page scheleton

---
+++ src/components/pages/home/HomePage.vue
@@ -0,0 +1,19 @@
+<script>
+
+import TheHeader from '../../layout/TheHeader.vue';
+import TheFooter from '../../layout/TheFooter.vue';
+
+export default {
+    components: {
+        TheHeader,
+        TheFooter
+    }
+}
+</script>
+
+<template>
+    <TheHeader></TheHeader>
+    <h1>In home page</h1>
+    <TheFooter></TheFooter>
+</template>
+
