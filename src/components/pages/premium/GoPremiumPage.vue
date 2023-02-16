ba3c6be1fc677c60622c2b0b129ac9df6f8772a9
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 23:38:41 +0200

    Added premium page scheleton

---
+++ src/components/pages/premium/GoPremiumPage.vue
@@ -0,0 +1,17 @@
+<script>
+
+import TheHeader from '../../layout/TheHeader.vue';
+import TheFooter from '../../layout/TheFooter.vue';
+
+export default {
+    components: {
+        TheHeader,
+        TheFooter
+    }
+}
+</script>
+<template>
+    <TheHeader></TheHeader>
+    <h1>On premium page</h1>
+    <TheFooter></TheFooter>
+</template>
