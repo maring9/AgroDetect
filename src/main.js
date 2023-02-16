a0ed270d931b578eae489f88bc6d73aae3c5cab2
Marin <marin.getejanc.upt@gmail.com>
2023-02-16 09:07:04 +0200

    Applied changes to the app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import router from './router.js';
 import './assets/main.css'
 
 import { Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
+import BaseCard from './components/ui/BaseCard.vue';
+
 Amplify.configure(awsExports);
-createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(router);
+
+app.component('BaseCard', BaseCard);
+
+app.mount('#app');
